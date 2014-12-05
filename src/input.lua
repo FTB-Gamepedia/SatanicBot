15dd4f727b1a4b05d4d55f6d2cc6388ee41791e5
satanicsanta <elifosterwy@gmail.com>
2014-12-04 23:45:24 -0800

    got started on nonpython nonjava rewrite

---
+++ src/input.lua
@@ -0,0 +1,8 @@
+MAJOR = 0
+MINOR = 1
+PATCH = 0
+
+CURRENT_CHANNEL = "#FTB-Wiki"
+
+io.write("Weeeee" .. MAJOR .. "." .. MINOR .. "." PATCH "\n")
+io.write("Socket handle: " .. _socket_connect("irc.esper.net", 6667) .. "\n");
