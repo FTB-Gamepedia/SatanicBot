6e2a471c140751bbcb4212d8a0ea1b7080672a86
satanicsanta <elifosterwy@gmail.com>
2014-12-06 12:43:08 -0800

    remvoe exit 0;

--- src/irc.pl
+++ src/irc.pl
@@ -4,24 +4,25 @@ use strict;
 
 package SatanicBot;
 use base qw(Bot::BasicBot);
+use POE qw(Component::IRC);
 
 my $bot = Bot::BasicBot->new(
-server    => 'irc.esper.net',
-port      => '6667',
-channels  => ['#SatanicSanta'],
+  server    => 'irc.esper.net',
+  port      => '6667',
+  channels  => ['#SatanicSanta'],
 
-nick      => 'SatanicBot',
-alt_nicks => ['SatanicButt', 'SatanicBooty'],
-username  => 'SatanicBot',
-name      => 'SatanicSanta\'s IRC bot'
+  nick      => 'SatanicBot',
+  alt_nicks => ['SatanicButt', 'SatanicBooty'],
+  username  => 'SatanicBot',
+  name      => 'SatanicSanta\'s IRC bot'
 )->run();
+
 
 #Use this subroutine definition for adding commands.
 sub said{
   my ($self, $message) = @_;
   if ($message->{body} eq '!quit'){
     $bot->shutdown($bot->quit_message('Someone killed me!!'));
-    exit 0;
   }
   if ($message->{body} eq '!abbrv'){
     $self->say($message->{channel}, $message->{'Bitch, that code ain\'t even work yet'})
