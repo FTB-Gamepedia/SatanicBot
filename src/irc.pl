f4784e9789111acb09a508ebea85b6c80698a9eb
satanicsanta <elifosterwy@gmail.com>
2014-12-07 22:03:16 -0800

    spacing convention

--- src/irc.pl
+++ src/irc.pl
@@ -13,101 +13,101 @@ require 'wiki.pl';
 my $chan = '#SatanicSanta';
 
 my $bot = SatanicBot->new(
-  server    => 'irc.esper.net',
-  port      => '6667',
-  channels  => [$chan],
+    server    => 'irc.esper.net',
+    port      => '6667',
+    channels  => [$chan],
 
-  nick      => 'SatanicBot|dev',
-  alt_nicks => ['SatanicButt', 'SatanicBooty'],
-  username  => 'SatanicBot',
-  name      => 'SatanicSanta\'s IRC bot'
+    nick      => 'SatanicBot|dev',
+    alt_nicks => ['SatanicButt', 'SatanicBooty'],
+    username  => 'SatanicBot',
+    name      => 'SatanicSanta\'s IRC bot'
 )->run();
 
 #Use this subroutine definition for adding commands.
 sub said{
-  my ($self, $message) = @_;
+    my ($self, $message) = @_;
 
-  #quit command: no args
-  if ($message->{body} eq '$quit'){
-    $bot->shutdown();
-  }
-
-  #abbrv command: 2 args required: <abbreviation> <mod name>
-  my $msg = $message->{body};
-  our @words = split(/\s/, $msg, 3);
-  if ($words[0] eq '$abbrv'){
-    if ($words[1] =~ m/.+/){
-      $self->say(
-        channel => $chan,
-        body    => "Abbreviating $words[2] as $words[1]"
-      );
-
-      Wiki->login();
-      Wiki->edit_gmods();
-      Wiki->logout();
-
-      $self->say(
-        channel => $chan,
-        body    => 'Abbreviation and documentation added.'
-      );
-      return undef;
-    } else {
-      $self->say(
-        channel => $chan,
-        body    => 'Please provide the required arguments.'
-      );
+    #quit command: no args
+    if ($message->{body} eq '$quit'){
+        $bot->shutdown();
     }
-  }
 
-  if ($message->{body} eq '$spookyscaryskeletons'){
-    my @random_words = rand_words(
-      wordlist => 'spook.lines',
-      min      => 10,
-      max      => 20
-    );
+    #abbrv command: 2 args required: <abbreviation> <mod name>
+    my $msg = $message->{body};
+    our @words = split(/\s/, $msg, 3);
+    if ($words[0] eq '$abbrv'){
+        if ($words[1] =~ m/.+/){
+            $self->say(
+                channel => $chan,
+                body    => "Abbreviating $words[2] as $words[1]"
+        );
 
-    $self->say(
-      channel => $chan,
-      body    => @random_words
-    );
-  }
+            Wiki->login();
+            Wiki->edit_gmods();
+            Wiki->logout();
 
-  my $weathermsg = $message->{body};
-  our @weatherwords = split(/\s/, $weathermsg, 2);
-  if ($weatherwords[0] eq '$weather'){
-    if ($weatherwords[1] =~ m/.+/){
-      my $weather = Weather::Underground::Forecast->new(
-        location => $weatherwords[1]
-      );
-
-      my $high   = $weather->highs;
-      my $low    = $weather->lows;
-      my $precip = $weather->precipitation;
-      #my $dump = Data::Dumper->new([$high]);
-      $self->say(
-        channel => $chan,
-        body    => 'High today: ' . $high->[0] . ' F'
-      );
-      $self->say(
-        channel => $chan,
-        body    => 'Low today: ' . $low->[0] . ' F'
-      );
-      $self->say(
-        channel => $chan,
-        body    => 'Chance of precipitation today: ' . $precip->[0] . '%'
-      );
-    } else {
-      $self->say(
-        channel => $chan,
-        body    => 'Please provide the required arguments.'
-      )
+            $self->say(
+                channel => $chan,
+                body    => 'Abbreviation and documentation added.'
+            );
+            return undef;
+        } else {
+            $self->say(
+                channel => $chan,
+                body    => 'Please provide the required arguments.'
+            );
+        }
     }
-  }
 
-  if ($message->{body} eq '$help'){
-    $self->say(
-      channel => $chan,
-      body    => 'Listing commands... quit, abbrv, spookyscaryskeletons, weather'
+    if ($message->{body} eq '$spookyscaryskeletons'){
+        my @random_words = rand_words(
+            wordlist => 'spook.lines',
+            min      => 10,
+            max      => 20
+        );
+
+        $self->say(
+            channel => $chan,
+            body    => @random_words
+        );
+    }
+
+    my $weathermsg = $message->{body};
+    our @weatherwords = split(/\s/, $weathermsg, 2);
+    if ($weatherwords[0] eq '$weather'){
+        if ($weatherwords[1] =~ m/.+/){
+            my $weather = Weather::Underground::Forecast->new(
+            location => $weatherwords[1]
+        );
+
+        my $high   = $weather->highs;
+        my $low    = $weather->lows;
+        my $precip = $weather->precipitation;
+        #my $dump = Data::Dumper->new([$high]);
+        $self->say(
+            channel => $chan,
+            body    => 'High today: ' . $high->[0] . ' F'
+        );
+        $self->say(
+            channel => $chan,
+            body    => 'Low today: ' . $low->[0] . ' F'
+        );
+        $self->say(
+            channel => $chan,
+            body    => 'Chance of precipitation today: ' . $precip->[0] . '%'
+        );
+    } else {
+        $self->say(
+            channel => $chan,
+            body    => 'Please provide the required arguments.'
+        );
+        }
+    }
+
+    if ($message->{body} eq '$help'){
+        $self->say(
+        channel => $chan,
+        body    => 'Listing commands... quit, abbrv, spookyscaryskeletons, weather'
     );
-  }
+    }
 }
--- src/wiki.pl
+++ src/wiki.pl
@@ -10,49 +10,49 @@ my $mw = MediaWiki::API->new();
 $mw->{config}->{api_url} = 'http://ftb.gamepedia.com/api.php';
 
 sub login{
-  my $file = 'secure.txt';
-  open my $fh, '<', $file or die "Could not open '$file' $!\n";
-  my @lines = <$fh>;
-  close $fh;
-  chomp @lines;
-  $mw->login({
-    lgname     => $lines[0],
-    lgpassword => $lines[-1]
-  }) || die $mw->{error}->{code} . ": " . $mw->{error}->{details};
+    my $file = 'secure.txt';
+    open my $fh, '<', $file or die "Could not open '$file' $!\n";
+    my @lines = <$fh>;
+    close $fh;
+    chomp @lines;
+    $mw->login({
+        lgname     => $lines[0],
+        lgpassword => $lines[-1]
+    }) || die $mw->{error}->{code} . ": " . $mw->{error}->{details};
 }
 
 sub edit_gmods{
-  my $gmods     = "Template:G/Mods";
-  my $gmodsdoc  = "Template:G/Mods/doc";
-  my $firstref  = $mw->get_page({title => $gmods});
-  my $secondref = $mw->get_page({title => $gmodsdoc});
-  my $replace_t = $firstref->{'*'};
-  my $replace_d = $secondref->{'*'};
+    my $gmods     = "Template:G/Mods";
+    my $gmodsdoc  = "Template:G/Mods/doc";
+    my $firstref  = $mw->get_page({title => $gmods});
+    my $secondref = $mw->get_page({title => $gmodsdoc});
+    my $replace_t = $firstref->{'*'};
+    my $replace_d = $secondref->{'*'};
 
-  unless ($firstref->{missing}){
-    $replace_t =~ s/\|#default/\|$SatanicBot::words[1] = {{#if:{{{name\|}}}{{{code\|}}}\|\|_(}}{{#if:{{{name\|}}}{{{link\|}}}\|$SatanicBot::words[2]\|$SatanicBot::words[1]}}{{#if:{{{name\|}}}{{{code\|}}}\|\|)}}\n\|$SatanicBot::words[2] = {{#if:{{{name\|}}}{{{code\|}}}\|\|_(}}{{#if:{{{name\|}}}{{{link\|}}}\|$SatanicBot::words[2]\|$SatanicBot::words[1]}}{{#if:{{{name\|}}}{{{code\|}}}\|\|)}}\n\n\|#default/;
-    $mw->edit({
-      action     => 'edit',
-      title      => $gmods,
-      text       => $replace_t,
-      bot        => 1,
-      minor      => 1
-    }) || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
-  }
+    unless ($firstref->{missing}){
+        $replace_t =~ s/\|#default/\|$SatanicBot::words[1] = {{#if:{{{name\|}}}{{{code\|}}}\|\|_(}}{{#if:{{{name\|}}}{{{link\|}}}\|$SatanicBot::words[2]\|$SatanicBot::words[1]}}{{#if:{{{name\|}}}{{{code\|}}}\|\|)}}\n\|$SatanicBot::words[2] = {{#if:{{{name\|}}}{{{code\|}}}\|\|_(}}{{#if:{{{name\|}}}{{{link\|}}}\|$SatanicBot::words[2]\|$SatanicBot::words[1]}}{{#if:{{{name\|}}}{{{code\|}}}\|\|)}}\n\n\|#default/;
+        $mw->edit({
+            action     => 'edit',
+            title      => $gmods,
+            text       => $replace_t,
+            bot        => 1,
+            minor      => 1
+        }) || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+    }
 
-  unless ($secondref->{missing}){
-    $replace_d =~ s/\|\}/\|-\n\| [[$SatanicBot::words[2]]] \|\| <code>$SatanicBot::words[1]<\/code>\n\|\}/;
-    $mw->edit({
-      action => 'edit',
-      title  => $gmodsdoc,
-      text   => $replace_d,
-      bot    => 1,
-      minor  => 1
-    }) || die $mw->{error}->{code} . ": " . $mw->{error}->{details};
-  }
+    unless ($secondref->{missing}){
+        $replace_d =~ s/\|\}/\|-\n\| [[$SatanicBot::words[2]]] \|\| <code>$SatanicBot::words[1]<\/code>\n\|\}/;
+        $mw->edit({
+            action => 'edit',
+            title  => $gmodsdoc,
+            text   => $replace_d,
+            bot    => 1,
+            minor  => 1
+        }) || die $mw->{error}->{code} . ": " . $mw->{error}->{details};
+    }
 }
 
 sub logout{
-  $mw->logout();
+    $mw->logout();
 }
 return 1;
