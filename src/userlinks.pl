3d4ddaf955efae9efed36342d8201d26da89f558
satanicsanta <elifosterwy@gmail.com>
2014-12-18 08:19:02 -0800

    commit

--- src/userlinks.pl
+++ src/userlinks.pl
@@ -5,18 +5,23 @@ use warnings;
 use diagnostics;
 use strict;
 use MediaWiki::Bot;
+use MediaWiki::API;
 
-my $mw = MediaWiki::Bot({
+my $mw = MediaWiki::Bot->new({
+    protocol => 'http',
     host     => 'ftb.gamepedia.com',
+    path     => '/',
     operator => 'TheSatanicSanta',
     debug    => 2
 });
+my $mwapi = MediaWiki::API->new();
+$mwapi->{config}->{api_url} = 'http://ftb.gamepedia.com/api.php';
 login();
 what();
 logout();
 
 sub login{
-    my $file = 'secure.txt';
+    my $file = 'info/secure.txt';
     open my $fh, '<', $file or die "Could not open '$file' $!\n";
     my @lines = <$fh>;
     close $fh;
@@ -25,34 +30,55 @@ sub login{
         username => $lines[0],
         password => $lines[-1]
     });
+    $mwapi->login({
+        lgname     => $lines[0],
+        lgpassword => $lines[-1]
+    });
 }
 
 sub what{
-    my @links  = $mw->what_links_here("User:SatanicSanta");
-    my @tlinks = $mw->what_links_here("User talk:SatanicSanta");
+    #my $links  = $mw->what_links_here("User:SatanicSanta");
+    #my $tlinks = $mw->what_links_here("User talk:SatanicSanta");
 
-    #This does not work.
-    my $text  = $mw->get_text(@{$links});
-    my $ttext = $mw->get_text(@{$tlinks});
+    my $firstref  = $mwapi->get_page({title => "User:TheSatanicSanta/Sandbox/userlinks"});
+    my $secondref = $mwapi->get_page({title => "User:TheSatanicSanta/Sandbox/userlinks"});
+    my $replace_user = $firstref->{'*'};
+    my $replace_talk = $secondref->{'*'};
 
-    $text  =~ s/[[User:SatanicSanta]]/[[User:TheSatanicSanta]]/;
-    $ttext =~ s/[[User talk:SatanicSanta]]/[[User talk:TheSatanicSanta]]/;
+    $replace_user =~ s/[[User:SatanicSanta]]/[[User:TheSatanicSanta]]/;
+    $replace_talk =~ s/[[User talk:SatanicSanta]]/[[User talk:TheSatanicSanta]]/;
 
-    $mw->edit({
-        page => @links,
-        text => $text,
-        summary => 'Fixing old user page links',
-        minor => 1,
-        bot => 1
-    });
+    $mwapi->edit({
+        action     => 'edit',
+        title      => "User:TheSatanicSanta/Sandbox/userlinks",
+        text       => $replace_user,
+        bot        => 1,
+        minor      => 1
+    }) || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
 
-    $mw->edit({
-        page    => @tlinks,
-        text    => $ttext,
-        summary => 'Fixing old talk page links',
-        minor   => 1,
-        bot     => 1
-    });
+    $mwapi->edit({
+        action     => 'edit',
+        title      => "User:TheSatanicSanta/Sandbox/userlinks",
+        text       => $replace_talk,
+        bot        => 1,
+        minor      => 1
+    }) || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+
+    #$mw->edit({
+    #    page    => 'User:TheSatanicSanta/Sandbox/userlinks',
+    #    text    => $replace_user,
+    #    summary => 'Fixing old user page links',
+    #    minor   => 1,
+    #    bot     => 1
+    #});
+
+    #$mw->edit({
+    #    page    => 'User:TheSatanicSanta/Sandbox/userlinks',
+    #    text    => $replace_talk,
+    #    summary => 'Fixing old talk page links',
+    #    minor   => 1,
+    #    bot     => 1
+    #});
 }
 
 sub logout{
