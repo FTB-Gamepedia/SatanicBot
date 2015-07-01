959ea47a65caa3a4f30c72b6ab6b21611eadcbd5
Eli Foster <elifosterwy@gmail.com>
2015-07-01 10:57:35 -0700

    Add get_backlinks method

--- src/ruby/wikiutils.rb
+++ src/ruby/wikiutils.rb
@@ -23,5 +23,24 @@ module Wiki_Utils
         @debug ? response : nil
       end
     end
+    
+    def get_backlinks(bltitle, blnamespace)
+      params = {
+        action: 'query',
+        list: 'backlinks',
+        bltitle: bltitle,
+        blnamespace: blnamespace
+        bllimit: 5000,
+      }
+      
+      request = URI(@api_page)
+      request.query = URI.encode_www_form(params)
+      response = Net::HTTP.get_response(request)
+      if response.is_a? Net::HTTPSuccess
+        return response.body
+      else
+        @debug ? response : nil
+      end
+    end
   end
 end
