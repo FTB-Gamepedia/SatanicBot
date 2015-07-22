eda1aa810cde1267e0adbb2d1a629357ec2c7378
elifoster <elifosterwy@gmail.com>
2015-07-21 19:10:23 -0700

    General fixes idk

--- src/SatanicBot/animalcats.rb
+++ src/SatanicBot/animalcats.rb
@@ -18,7 +18,29 @@ def change_pages(category, new_category_name)
   end
 end
 
+def change_backlinks(category, new_category_name)
+  backlinkarray = []
+  JSON.parse($other_mw.get_backlinks("Category:#{category}"))["query"]["backlinks"].each do |title|
+    backlinkarray.push(title["title"])
+  end
+  backlinkarray.each do |i|
+    if $other_mw.get_wikitext(i) == false
+      puts i + " could not be edited because its content is nil. Continuing...\n"
+      next
+    else
+      text = $other_mw.get_wikitext(i)
+      text = text.gsub(/\{\{C\|#{category}/, "\{\{C\|#{new_category_name}")
+      text = text.gsub(/#{category}/, "#{new_category_name}")
+      $mw.edit(title: i, text: text, bot: 1, summary: "Changing #{category} to #{new_category_name}")
+      puts "#{i} has been edited.\n"
+    end
+  end
+end
+
 change_pages("Category:Passive Animals", "Category:Passive Creatures")
 change_pages("Category:Angry Monsters", "Category:Hostile Creatures")
 change_pages("Category:Neutral Animals", "Category:Neutral Creatures")
+change_backlinks("Passive Animals", "Passive Creatures")
+change_backlinks("Angry Monsters", "Hostile Creatures")
+change_backlinks("Neutral Animals", "Neutral Creatures")
 exit
