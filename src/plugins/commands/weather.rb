5f659a5910befbf99498b61084c68a60ba3b6d6f
elifoster <elifosterwy@gmail.com>
2015-10-17 10:54:15 -0700

    Alerts are now included in the  command.

--- src/plugins/commands/weather.rb
+++ src/plugins/commands/weather.rb
@@ -2,6 +2,7 @@ module Plugins
   module Commands
     class Weather
       include Cinch::Plugin
+      # include Cinch::Formatting
 
       match(/weather (.+)/i, method: :weather)
       match(/forecast (.+)/i, method: :forecast)
@@ -9,9 +10,18 @@ module Plugins
       def weather(msg, location)
         weather = LittleHelper.init_weather
         conditions = weather.conditions(location)
-        msg.reply("#{conditions[:full_name]}: #{conditions[:weather]} | " \
-                  "#{conditions[:formatted_temperature]} | Humidity: " \
-                  "#{conditions[:humidity]}% | #{conditions[:updated]}")
+        alerts = weather.alerts(location)
+        message = "#{conditions[:full_name]}: #{conditions[:weather]} | " \
+                       "#{conditions[:formatted_temperature]} | Humidity: " \
+                       "#{conditions[:humidity]}% | #{conditions[:updated]}"
+        unless alerts.nil?
+          alert_hash = {}
+          alerts.each do |a|
+            desc = Cinch::Formatting.format(:red, a[:description])
+            message = message + " | #{desc} until #{a[:expires]}"
+          end
+        end
+        msg.reply(message)
       end
 
       def forecast(msg, location)
