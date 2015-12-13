b667ec576a3f99e8f48d8b1f560378f23f38813b
elifoster <elifosterwy@gmail.com>
2015-12-13 00:40:59 -0800

    :bug: Pull requests are properly recognized.

--- src/plugins/commands/issue_link.rb
+++ src/plugins/commands/issue_link.rb
@@ -9,11 +9,12 @@ module Plugins
 
       match(/([\d]+)/, method: :default_repo, prefix: /\#/)
 
-      def form_message(state, title, labels, number)
+      def form_message(state, title, labels, number, is_pull)
+        start = is_pull ? 'The pull request' : 'The issue'
         if labels.empty?
-          "The issue, \"#{title}\" ##{number}, is #{state}."
+          "#{start}, \"#{title}\" ##{number}, is #{state}."
         else
-          "The issue, \"#{title}\" ##{number}, is #{state}, with the " \
+          "#{start}, \"#{title}\" ##{number}, is #{state}, with the " \
           "following labels: #{labels.join(', ')}"
         end
       end
@@ -29,14 +30,16 @@ module Plugins
         repo = Variables::Constants::ISSUE_TRACKING[msg.channel]
         begin
           issue = Octokit.issue(repo, issue_num)
+          is_pull = issue.key?(:pull_request)
           state = issue['state']
           title = issue['title']
           labels = []
           issue['labels'].each do |l|
             labels << l['name']
           end
-          msg.reply("https://github.com/#{repo}/issues/#{issue_num}")
-          msg.reply(form_message(state, title, labels, issue_num))
+          url_type = is_pull ? 'pull' : 'issues'
+          msg.reply("https://github.com/#{repo}/#{url_type}/#{issue_num}")
+          msg.reply(form_message(state, title, labels, issue_num, is_pull))
         rescue Octokit::NotFound
           msg.reply("Issue ##{issue_num} cannot be found.")
         end
@@ -44,12 +47,15 @@ module Plugins
 
       def repo_syntax(msg, user, repo, num)
         issue = Octokit.issue("#{user}/#{repo}", num)
+        is_pull = issue.key?(:pull_request)
         labels = []
         issue['labels'].each do |label|
           labels << label['name']
         end
-        msg.reply("https://github.com/#{user}/#{repo}/issues/#{num}")
-        msg.reply(form_message(issue['state'], issue['title'], labels, num))
+        url_type = is_pull ? 'pull' : 'issues'
+        msg.reply("https://github.com/#{user}/#{repo}/#{url_type}/#{num}")
+        msg.reply(form_message(issue['state'], issue['title'], labels, num,
+                               is_pull))
       rescue Octokit::NotFound
         msg.relpy("Issue ##{issue} cannot be found.")
       end
