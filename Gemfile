c7d4406c7729927ef5b097e8dd60b9679151a590
elifoster <elifosterwy@gmail.com>
2016-01-05 11:14:24 -0800

    :gem: :bug: Shorten URLs with the isgd gem. Support multiple issues in a single message. Close #77.

--- Gemfile
+++ Gemfile
@@ -12,5 +12,6 @@ gem('simple_geolocator', '>= 1.3.0')
 gem('octokit', '>= 4.2.0')
 gem('array_utility', '>= 1.1.0')
 gem('pastee', '>= 1.0.0')
+gem('isgd', '>= 1.0.0')
 
 ruby '2.2.3'
--- src/plugins/commands/issue_link.rb
+++ src/plugins/commands/issue_link.rb
@@ -1,5 +1,6 @@
 require 'cinch'
 require 'octokit'
+require 'isgd'
 require_relative '../../variables'
 
 module Plugins
@@ -13,15 +14,12 @@ module Plugins
       # @param state [String] The state of the issue (open/closed)
       # @param title [String] The issue's title.
       # @param number [String] The issue number.
-      # @param is_pull [Boolean] Whether the issue is a pull request, and not a
-      #   normal issue.
       # @return [String] The formatted message.
-      def form_message(state, title, labels, number, is_pull)
-        start = is_pull ? 'The pull request' : 'The issue'
+      def form_message(state, title, labels, number)
         if labels.empty?
-          "#{start}, \"#{title}\" ##{number}, is #{state}."
+          "\"#{title}\" ##{number}, is #{state}."
         else
-          "#{start}, \"#{title}\" ##{number}, is #{state}, with the " \
+          "\"#{title}\" ##{number}, is #{state}, with the " \
           "following labels: #{labels.join(', ')}"
         end
       end
@@ -32,26 +30,48 @@ module Plugins
       # @param msg [Cinch::Message]
       # @param issue_num [String] The GitHub issue number.
       def default_repo(msg, issue_num)
-        match = msg.message.match(/(\S+)\/(\S+)##{issue_num}/)
-        unless match.nil?
-          msg_data = match.to_s.split(/[\#\/]/)
-          repo_syntax(msg, msg_data[0], msg_data[1], msg_data[2])
-          return
+        multiple_match = msg.message.scan(/(?:[\W]+|^)(#[\d]+)/)
+        repo_message = msg.message.scan(/\S+\/\S+#\d+/)
+        channel_valid = Variables::Constants::ISSUE_TRACKING.include?(msg.channel)
+        message = []
+        if multiple_match.size > 0 && multiple_match[0].size > 0 && channel_valid
+          repo = Variables::Constants::ISSUE_TRACKING[msg.channel]
+          multiple_match[0].each do |i|
+            num = i.split(/[#]/)[1]
+            url = ISGD.shorten("https://github.com/#{repo}/issues/#{num}")
+            message << "#{repo}#{i}: #{url}"
+          end
         end
-        return unless Variables::Constants::ISSUE_TRACKING.include?(msg.channel)
+
+        if repo_message.size > 0
+          repo_message.each do |i|
+            msg_data = i.split(/[#]/)
+            url = ISGD.shorten("https://github.com/#{msg_data[0]}/issues/#{msg_data[1]}")
+            message << "#{msg_data[0]}##{msg_data[1]}: #{url}"
+          end
+        end
+
+        if message.any?
+          msg.reply(message.join(', '))
+          return if message.size > 1
+        end
+
+        unless repo_message.nil?
+          msg_data = repo_message[0].to_s.split(/[#\/]/)
+          repo_syntax(msg, msg_data[0], msg_data[1], msg_data[2])
+        end
+
+        return unless channel_valid
         repo = Variables::Constants::ISSUE_TRACKING[msg.channel]
         begin
           issue = Octokit.issue(repo, issue_num)
-          is_pull = issue.key?(:pull_request)
           state = issue['state']
           title = issue['title']
           labels = []
           issue['labels'].each do |l|
             labels << l['name']
           end
-          url_type = is_pull ? 'pull' : 'issues'
-          msg.reply("https://github.com/#{repo}/#{url_type}/#{issue_num}")
-          msg.reply(form_message(state, title, labels, issue_num, is_pull))
+          msg.reply(form_message(state, title, labels, issue_num,))
         rescue Octokit::NotFound
           msg.reply("Issue ##{issue_num} cannot be found.")
         end
@@ -65,17 +85,13 @@ module Plugins
       # @param num [String] The issue number.
       def repo_syntax(msg, user, repo, num)
         issue = Octokit.issue("#{user}/#{repo}", num)
-        is_pull = issue.key?(:pull_request)
         labels = []
         issue['labels'].each do |label|
           labels << label['name']
         end
-        url_type = is_pull ? 'pull' : 'issues'
-        msg.reply("https://github.com/#{user}/#{repo}/#{url_type}/#{num}")
-        msg.reply(form_message(issue['state'], issue['title'], labels, num,
-                               is_pull))
+        msg.reply(form_message(issue['state'], issue['title'], labels, num))
       rescue Octokit::NotFound
-        msg.relpy("Issue ##{issue} cannot be found.")
+        msg.reply("Issue ##{issue} cannot be found.")
       end
     end
   end
