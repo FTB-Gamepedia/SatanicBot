58cc117cdebacb3cecafe50ff27d6e6bb9689e36
elifoster <elifosterwy@gmail.com>
2016-02-13 20:43:49 -0800

    :bug: Fix param name.

--- src/plugins/commands/refresh.rb
+++ src/plugins/commands/refresh.rb
@@ -9,7 +9,7 @@ module Plugins
       doc = 'Refreshes the quote list. Not necessary when adding quotes, as that will automatically append to the list.'
       Variables::NonConstants.add_command('refreshquotes', doc)
 
-      def execute(_)
+      def execute(msg)
         Variables::NonConstants.get_quotes(true)
         msg.reply('Finished refreshing quotes.')
       end
