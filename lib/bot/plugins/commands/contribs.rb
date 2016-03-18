dcc738d05091f3b157367601618cd3c3fbe14caf
elifoster <elifosterwy@gmail.com>
2016-03-18 15:39:33 -0700

    :bug: :fu: :racehorse: Fix #90 by empty checking. Improve line length style. Improve message_start assignment performance (benchmarked in Pry)

--- lib/bot/plugins/commands/contribs.rb
+++ lib/bot/plugins/commands/contribs.rb
@@ -15,16 +15,18 @@ module Plugins
             'IRC nickname.'.freeze
       Variables::NonConstants.add_command('contribs', DOC)
 
-      # Gets the amount of contributions and the registration date of the given
-      #   user.
+      # Gets the amount of contributions and the registration date of the given user.
       # @param msg [Cinch::Message]
       # @param username [String] The username to check.
-      # @param you [Boolean] Whether the username is also the user who performed
-      #   the command.
+      # @param you [Boolean] Whether the username is also the user who performed the command.
       def execute(msg, username, you = false)
         return if Variables::Constants::IGNORED_USERS.include?(msg.user.nick)
         butt = LittleHelper.init_wiki
         count = butt.get_contrib_count(username).to_s.separate
+        if count.empty?
+          msg.reply("#{username} is not a user on the wiki.")
+          return
+        end
         date = butt.get_registration_time(username)
         month = date.strftime('%B').strip
         day = date.strftime('%e').strip
@@ -33,26 +35,24 @@ module Plugins
         message_start =
           if you
             'You have'.freeze
-          elsif /[Ss]atanicBot/ =~ username
+          elsif username.downcase == 'satanicbot'
             'I have'.freeze
-          elsif /[Tt]heSatanicSanta/ =~ username
+          elsif username.downcase == 'thesatanicsanta'
             'The second hottest babe in the channel has'.freeze
-          elsif /[Rr]etep998/ =~ username
+          elsif username.downcase == 'retep998'
             'The hottest babe in the channel has'.freeze
-          elsif /[Pp]onyButt/ =~ username
+          elsif username.downcase == 'ponybutt'
             'Some stupid bitch has'.freeze
           else
             "#{username} has"
           end
 
         message_contribs = count == '1' ? '1 contribution' : "#{count} contributions"
-        message = "#{message_start} made #{message_contribs} to the wiki and " \
-                  "registered on #{month} #{day}, #{year}"
+        message = "#{message_start} made #{message_contribs} to the wiki and registered on #{month} #{day}, #{year}"
         msg.reply(message)
       end
 
-      # Gets the amount of contributions and the registration date of the user
-      #   who performed the command, by their IRC nickname.
+      # Gets the amount of contributions and the registration date of the user who performed the command.
       # @see execute
       # @param msg [Cinch::Message]
       def no_username(msg)
