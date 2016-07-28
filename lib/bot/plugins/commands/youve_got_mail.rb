717b561ce6d0515f789b03d36a1d08439cd22a60
elifoster <elifosterwy@gmail.com>
2016-07-28 11:12:17 -0700

    :wrench: State the amount of unread messages in the YouveGotMail message. Also bony your line was too long :fu:

--- lib/bot/plugins/commands/youve_got_mail.rb
+++ lib/bot/plugins/commands/youve_got_mail.rb
@@ -15,7 +15,10 @@ module Plugins
         nick = user.nick
         nick_dc = nick.downcase
         auth = user.authname&.downcase
-        msg.channel.send("#{nick}: You've got mail! Use the checkmail command to check your mail!") if table.where(to: [nick_dc, auth]).count > 0
+        count = table.where(to: [nick_dc, auth]).count
+        if count > 0
+          msg.channel.send("#{nick}: You've got mail (#{count} unread)! Use the checkmail command to check your mail!")
+        end
       end
     end
   end
