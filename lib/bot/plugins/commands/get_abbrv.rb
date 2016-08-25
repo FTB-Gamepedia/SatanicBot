af3f99c83217adc7f6abaf38333aeab9141cee92
elifoster <elifosterwy@gmail.com>
2016-08-24 22:52:04 -0700

    :bug: :fu: $getabbrv fix to handle multiple identical mods/abbreviations. Clean up GetAbbreviation a lot.

--- lib/bot/plugins/commands/get_abbrv.rb
+++ lib/bot/plugins/commands/get_abbrv.rb
@@ -13,6 +13,34 @@ module Plugins
 
       PAGE = 'Module:Mods/list'.freeze
 
+      # @param text [String] The text to scan
+      # @param standard [Regexp] The regular expression to scan as the "standard" syntax (ABBRV = {'Name'})
+      # @param hyphen [Regexp] the regular expression to scan as the "hyphen" syntax (["AB-BRV"] = {'Name'})
+      # @return [Array<String>] All of the matches to the two scans, combined into a single dimensional array.
+      def get_matches(text, standard, hyphen)
+        standard_match = text.scan(standard)
+        hyphen_match = text.scan(hyphen)
+
+        standard_match = [] unless standard_match[0]
+        hyphen_match = [] unless hyphen_match[0]
+
+        standard_match.flatten.concat(hyphen_match.flatten)
+      end
+
+      # @param mod [String] The mod name
+      # @param text [String] The text in the PAGE.
+      # @return [Array<String>] An array of all of the valid abbreviations for the given mod.
+      def get_abbreviations(mod, text)
+        get_matches(text, /\s{1,}([A-Z0-9\-]+) = {'#{mod}',/, /\s{1,}\["([A-Z0-9\-]+)"\] = {'#{mod}',/)
+      end
+
+      # @param abbreviation [String] The abbreviation
+      # @param text [String] The text in the PAGE
+      # @return [Array<String>] The names for the abbreviation. This should probably not be longer than 1.
+      def get_names(abbreviation, text)
+        get_matches(text, /\s{1,}#{abbreviation} = {'(.+)',/, /\s{1,}\["#{abbreviation}\"\] = {'(.+)',/)
+      end
+
       # Gets either the abbreviation of a mod, or the mod using an abbreviation.
       # @param msg [Cinch::Message]
       # @param thing [String] The abbreviation OR mod.
@@ -21,23 +49,17 @@ module Plugins
         butt = LittleHelper.init_wiki
         module_text = butt.get_text(PAGE)
         thing.gsub!(/'/) { "\\'" }
-        if module_text.include?(thing)
-          if module_text =~ /[\s+]#{thing} = \{\'/
-            match_data = module_text.scan(/\s{1,}#{thing} = {'(.+)',/)
-            mod_name = match_data[0][0]
-            msg.reply("#{thing} is the abbreviation for #{mod_name}")
-          elsif module_text.include?("= {'#{thing}',")
-            thing.gsub!(/'/) { "\\'" }
-            match_data = module_text.scan(/([A-Z0-9\-]+) = {'#{thing}',/)
-            msg.reply("#{thing.gsub(/\\'/) { "'" }} is abbreviated as #{match_data[0][0]}")
-          else
-            msg.reply("#{thing} does not appear to be in the abbreviation " \
-                      'list, but there were similar non-exact results. ' \
-                      'Try something similar.')
-          end
-        else
-          msg.reply("#{thing} does not appear to be in the abbreviation list.")
-        end
+
+        names = get_names(thing, module_text)
+        abbreviations = get_abbreviations(thing, module_text)
+
+        thing = thing.gsub(/\\'/) { "'" }
+        replies = []
+        replies << "#{thing} does not appear to be in the abbreviation list." if names.empty? && abbreviations.empty?
+        replies << "#{thing} is the abbreviation for the following mod(s): #{names.join(', ')}" unless names.empty?
+        replies << "#{thing} is abbreviated as the following: #{abbreviations.join(', ')}" unless abbreviations.empty?
+
+        replies.map { |str| msg.reply(str) }
       end
     end
   end
