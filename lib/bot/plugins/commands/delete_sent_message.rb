42234b1da7008caa3e52585675895d033fad54b1
elifoster <elifosterwy@gmail.com>
2016-09-10 12:48:07 -0700

    :bug: Improve SQL stuff

--- lib/bot/plugins/commands/delete_sent_message.rb
+++ lib/bot/plugins/commands/delete_sent_message.rb
@@ -10,15 +10,13 @@ module Plugins
       def execute(msg, mail_id)
         table = LittleHelper.message_table
         user = msg.user
-        to_delete = table.where(id: mail_id)
+        to_delete = table.where(id: mail_id, from: [user.nick, user.authname]).all
 
         if to_delete.count == 0
-          msg.reply("No messages found with ID #{mail_id}")
-        elsif to_delete[:from] == user.nick.downcase || to_delete[:from] == user.authname&.downcase
+          msg.reply("No messages found with ID #{mail_id} sent by you")
+        else to_delete[:from] == user.nick.downcase || to_delete[:from] == user.authname&.downcase
           to_delete.delete
           msg.reply("Deleted message #{mail_id}")
-        else
-          msg.reply("Could not delete #{mail_id} because you did not send that message.")
         end
       end
     end
