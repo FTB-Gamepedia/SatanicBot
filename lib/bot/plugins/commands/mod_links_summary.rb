877c5409df0f0290c20ab0125c4b5b051e19ff13
elifoster <elifosterwy@gmail.com>
2017-08-30 18:54:03 -0700

    :wrench: Refactor mod links summary code to be more modular This will make it significantly easier to special case categories that should have their own section in the list

--- lib/bot/plugins/commands/mod_links_summary.rb
+++ lib/bot/plugins/commands/mod_links_summary.rb
@@ -10,13 +10,19 @@ module Plugins
       set(help: 'Gets a summary of problematic links within a mod page. 1 arg: $modlinkssummary <page name>')
       match(/modlinkssummary (.+)/)
 
-      def create_paste(page_name, disambiguation_pages, other_mod_pages)
-        str = "== Disambiguation pages ==\n"
-        str << disambiguation_pages.join("\n")
-        str << "\n\n"
-        str << "== Pages not for this mod ==\n"
+      SPECIAL_CATEGORIES = [
+        'Disambiguation pages'
+      ].freeze
+
+      def create_paste(page_name, special_category_pages, other_mod_pages)
+        str = "== Pages not for this mod ==\n"
         str << other_mod_pages.join("\n")
         str << "\n\n"
+        special_category_pages.each do |category, pages|
+          str << "== #{category} ==\n"
+          str << pages.join("\n")
+          str << "\n\n"
+        end
 
         LittleHelper::PASTEE.submit(str, "Summary of bad links in #{page_name}")
       end
@@ -28,24 +34,20 @@ module Plugins
           msg.reply('This page does not exist.')
           return
         end
-        category = mw.get_categories_in_page(page_name).select do |category|
+        links_in_page.select! { |title| mw.get_text(title) }
+        mod_cat_for_page = mw.get_categories_in_page(page_name).select do |category|
           mw.get_categories_in_page(category).include?('Category:Mods')
         end[0]
-        all_disambiguation_pages = mw.get_category_members('Disambiguation pages')
-        disambiguation_pages_in_page = []
-        other_mod_pages_in_page = []
-        links_in_page.each do |title|
-          next unless mw.get_text(title)
-          disambiguation_pages_in_page << title if all_disambiguation_pages.include?(title)
-          if category
-            other_mod_pages_in_page << title unless mw.get_categories_in_page(title).include?(category)
-          end
+        special_category_pages = Hash.new([])
+        SPECIAL_CATEGORIES.each do |special_category|
+          all_pages_in_category = mw.get_category_members(special_category)
+          special_category_pages[special_category] = links_in_page.select { |title| all_pages_in_category.include?(title) }
         end
+        p special_category_pages
+        other_mod_pages = links_in_page.reject { |title| mw.get_categories_in_page(title).include?(mod_cat_for_page) }
 
-        msg.reply("http://paste.ee/p/#{create_paste(page_name, disambiguation_pages_in_page, other_mod_pages_in_page)}")
+        msg.reply("http://paste.ee/p/#{create_paste(page_name, special_category_pages, other_mod_pages)}")
       end
     end
   end
-end
-class ModLinksSummary
 end
