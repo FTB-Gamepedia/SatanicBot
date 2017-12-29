b6004bf8be8ac25c0b0057dbf31e6ce36d72b713
elifoster <elifosterwy@gmail.com>
2017-12-29 15:59:23 -0800

    :gem: Add caching mechanism to urban dictionary command Close #137.

--- lib/bot/plugins/commands/urban_dictionary.rb
+++ lib/bot/plugins/commands/urban_dictionary.rb
@@ -11,14 +11,42 @@ module Plugins
       match(/what does ["'](.+)["'] mean\?/i, use_prefix: false)
       set(help: 'Gets a definition for the word from Urban Dictionary.', plugin_name: 'urban')
 
+      CACHE = {}
+
       def execute(msg, word)
+        validate_cache(msg.user)
         slangs = UrbanDictionary.define(word)
         if slangs.empty?
           msg.reply("That's a good question, #{msg.user.nick}.")
         else
-          chosen = slangs.sample
-          msg.reply("Well, #{msg.user.nick}, according to #{chosen.author}, it means '#{chosen.definition}'")
+          chosen = nil
+          slangs.each do |slang|
+            chosen = slang unless in_cache?(msg.user, slang)
+          end
+          if chosen
+            msg.reply("Well, #{msg.user.nick}, according to #{chosen.author}, it means '#{chosen.definition}'")
+            cache_definition(msg.user, chosen)
+          else
+            msg.reply("#{msg.user.nick}, you have exhausted all definitions for '#{word}'.")
+          end
         end
+      end
+
+      # @param user [User]
+      # @param slang [Slang]
+      def cache_definition(user, slang)
+        CACHE[user.nick] << slang.id
+      end
+
+      # @param user [User]
+      # @param slang [Slang]
+      def in_cache?(user, slang)
+        CACHE[user.nick].include?(slang.id)
+      end
+
+      # @param user [User]
+      def validate_cache(user)
+        CACHE[user.nick] = [] unless CACHE.key?(user.nick)
       end
     end
   end
